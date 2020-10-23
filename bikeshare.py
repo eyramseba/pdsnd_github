933d65d92ead0a92cfcb073f523d7b5f290bd969
seba kossi nutefe eyram <eyram.seba@gmail.com>
2020-10-23 21:29:37 +0000

    refactor: Modified def main

--- bikeshare.py
+++ bikeshare.py
@@ -228,12 +228,10 @@ def raw_data(df):
 
 def main():
     while True:
-        city, month, day = get_filters()
+        city = get_city()
+        month = get_month()
+        day = get_day()
         df = load_data(city, month, day)
-        #city = get_city()
-        #month = get_month()
-        #day = get_day()
-        #df = load_data(city, month, day)
 
         time_stats(df)
         station_stats(df)
